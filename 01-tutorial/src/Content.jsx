f7d1b8ae3562234b1fcec485b6102438c7a3c9c0
azeezabidoye <azeezabidoye@yahoo.com>
2025-10-17 04:52:38 +0100

    Update

--- 01-tutorial/src/Content.jsx
+++ 01-tutorial/src/Content.jsx
@@ -10,12 +10,16 @@ const Content = () => {
   const handleClick = () => {
     console.log("You clicked me!");
   };
+  const handleClick2 = (firstName) => {
+    console.log(`You clicked to see ${firstName}`);
+  };
   return (
     <main>
       <p>
         Today we celebrate <strong>{handleNameChange(names)}</strong> 🎉
       </p>
       <button onClick={handleClick}>Click Me</button>
+      <button onClick={() => handleClick2("Azeez")}>Click Me</button>
     </main>
   );
 };
