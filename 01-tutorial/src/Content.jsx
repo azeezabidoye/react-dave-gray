d0f3a6ce7def6b1cd6bd0f2388a2cab719a09176
azeezabidoye <azeezabidoye@yahoo.com>
2025-10-18 03:54:12 +0100

    Update

--- 01-tutorial/src/Content.jsx
+++ 01-tutorial/src/Content.jsx
@@ -2,28 +2,25 @@ import React from "react";
 import { useState } from "react";
 
 const Content = () => {
-  const [name, setName] = useState("Dave");
-  function handleNameChange() {
-    const names = ["Mata", "Ben", "Yunus"];
-    const int = Math.floor(Math.random() * names.length);
-    setName(names[int]);
-  }
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: true,
+      item: "One half pound bag of Cocoa Covered Almonds",
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "Item 2",
+    },
+    {
+      id: 3,
+      checked: false,
+      item: "Item 3",
+    },
+  ]);
 
-  const handleClick = () => {
-    return handleNameChange();
-  };
-  const handleClick2 = (firstName) => {
-    console.log(`You clicked to see ${firstName}`);
-  };
-  return (
-    <main>
-      <p>
-        Today we celebrate <strong>{name}</strong> 🎉
-      </p>
-      <button onClick={handleClick}>Change Name</button>
-      <button onClick={() => handleClick2("Azeez")}>Click Me</button>
-    </main>
-  );
+  return <main></main>;
 };
 
 export default Content;
