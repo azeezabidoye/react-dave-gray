e1cee177d89f01d2f3f12bef88e04ed82bff5683
azeezabidoye <azeezabidoye@yahoo.com>
2025-10-20 15:00:44 +0100

    Update

--- 01-tutorial/src/Content.jsx
+++ 01-tutorial/src/Content.jsx
@@ -37,28 +37,32 @@ const Content = () => {
 
   return (
     <main>
-      <ul>
-        {items.map((item) => (
-          <li key={item.id} className="item">
-            <input
-              type="checkbox"
-              checked={item.checked}
-              onChange={() => handleCheck(item.id)}
-            />
-            <label
-              onDoubleClick={() => handleCheck(item.id)}
-              style={item.checked ? { textDecoration: "line-through" } : null}
-            >
-              {item.itemName}
-            </label>
-            <FaTrash
-              role="button"
-              tabIndex={0}
-              onClick={() => handleDelete(item.id)}
-            />
-          </li>
-        ))}
-      </ul>
+      {items.length ? (
+        <ul>
+          {items.map((item) => (
+            <li key={item.id} className="item">
+              <input
+                type="checkbox"
+                checked={item.checked}
+                onChange={() => handleCheck(item.id)}
+              />
+              <label
+                onDoubleClick={() => handleCheck(item.id)}
+                style={item.checked ? { textDecoration: "line-through" } : null}
+              >
+                {item.itemName}
+              </label>
+              <FaTrash
+                role="button"
+                tabIndex={0}
+                onClick={() => handleDelete(item.id)}
+              />
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p style={{ marginTop: "2rem" }}>Your list is empty!</p>
+      )}
     </main>
   );
 };
