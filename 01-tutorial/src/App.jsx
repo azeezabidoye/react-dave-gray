1f4c31ac5e4ac3a19d8ada716f7d1e35f3b2ce3e
azeezabidoye <azeezabidoye@yahoo.com>
2025-10-23 06:24:50 +0100

    Update

--- 01-tutorial/src/App.jsx
+++ 01-tutorial/src/App.jsx
@@ -5,23 +5,10 @@ import Content from "./Content";
 import Footer from "./Footer";
 
 function App() {
-  const [items, setItems] = useState([
-    {
-      id: 1,
-      checked: true,
-      itemName: "One half pound bag of Cocoa Covered Almonds",
-    },
-    {
-      id: 2,
-      checked: false,
-      itemName: "Item 2",
-    },
-    {
-      id: 3,
-      checked: false,
-      itemName: "Item 3",
-    },
-  ]);
+  const [items, setItems] = useState(() => {
+    const savedItems = localStorage.getItem("shoppinglist");
+    return savedItems ? JSON.parse(savedItems) : [];
+  });
 
   const [newItem, setNewItem] = useState("");
 
@@ -34,22 +21,19 @@ function App() {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
     const myNewItems = { id, checked: false, itemName: item };
     const listItems = [...items, myNewItems];
-    setItems(listItems);
-    localStorage.setItems("shoppinglist", JSON.stringify(listItems));
+    setAndSaveItems(listItems);
   };
 
   const handleCheck = (id) => {
     const listItems = items.map((item) =>
       item.id === id ? { ...item, checked: !item.checked } : item
     );
-    setItems(listItems);
-    localStorage.setItems("shoppinglist", JSON.stringify(listItems));
+    setAndSaveItems(listItems);
   };
 
   const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id);
-    setItems(listItems);
-    localStorage.setItems("shoppinglist", JSON.stringify(listItems));
+    setAndSaveItems(listItems);
   };
 
   const handleSubmit = (e) => {
