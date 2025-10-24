05237331f7788a7640e2b19bfc4d1588aa9fd65b
azeezabidoye <azeezabidoye@yahoo.com>
2025-10-24 07:11:22 +0100

    Update

--- 01-tutorial/src/SearchItem.jsx
+++ 01-tutorial/src/SearchItem.jsx
@@ -2,7 +2,15 @@ import React from "react";
 
 const SearchItem = () => {
   return (
-    <form className="searchForm" onSubmit={(e) => preventDefault()}></form>
+    <form className="searchForm" onSubmit={(e) => preventDefault()}>
+      <label htmlFor="search">Search</label>
+      <input
+        id="search"
+        type="text"
+        role="searchbox"
+        placeholder="Search item..."
+      />
+    </form>
   );
 };
 
